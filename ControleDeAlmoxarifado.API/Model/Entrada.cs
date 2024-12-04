7b72497541114b9f9058a48055189823e017f13c
andre-silva366 <andre.java.ads.2022@gmail.com>
2024-12-04 20:05:45 -0300

    Criação do projeto API e classes da Model

---
+++ ControleDeAlmoxarifado.API/Model/Entrada.cs
@@ -0,0 +1,15 @@
+﻿namespace ControleDeAlmoxarifado.API.Model;
+
+public class Entrada
+{
+    public int Id { get; set; }
+    public DateTime DataEntrada { get; set; }
+    public int IdProduto { get; set; }
+    public int Quantidade { get; set; }
+    public decimal PrecoUnitario { get; set; }
+    public int PreçoTotal { get; set; }
+    public int IdFornecedor { get; set; }
+    public Produto Produto { get; set; }
+    public Fornecedor Fornecedor { get; set; }
+    public Funcionario Funcionario { get; set; }
+}
---
+++ ControleDeAlmoxarifado.API/Model/Saida.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeAlmoxarifado.API.Model;
+
+public class Saida
+{
+    public int Id { get; set; }
+    public DateTime DataSaida { get; set; }
+    public int IdProduto { get; set; }
+    public int IdSolicitante { get; set; }
+    public int IdAlmoxarife { get; set; }
+    public int Quantidade { get; set; }
+}
